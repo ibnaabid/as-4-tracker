00f8d076c9a16bad3113bdf3c73588470ffd583c
MD Ibna Abid <mdabout2@gmail.com>
2026-02-24 12:50:33 +0600

    add id from html

---
+++ B13-A4-PH-Job-Tracker/job.js
@@ -0,0 +1,14 @@
+let interviewCount = [];
+let rejectedCount = [];
+
+let cardsContainer = document.getElementById("allcards");
+let filterContainer = document.getElementById("filter-section");
+const emptyMessage = document.getElementById("empty-message");
+
+let totalValue = document.getElementById("totalvalue");
+let interviewValue = document.getElementById("interviewvalue");
+let rejectedValue = document.getElementById("rejectedvalue");
+
+let totalBtn = document.getElementById("totalBtn");
+let interviewBtn = document.getElementById("interviewBtn");
+let rejectedBtn = document.getElementById("rejectedBtn");
